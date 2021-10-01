366e99fb40292969f9b0da4b04782bf03775ff67
Nurunnahar Nody <nurunnaharnody@gmail.com>
2021-10-01 10:25:37 +0600

    addded

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Inventory = () => {
     return (
         <div>
-            <h2>This is inventory</h2>
+            <h2>This is inventory ok</h2>
         </div>
     );
 };
